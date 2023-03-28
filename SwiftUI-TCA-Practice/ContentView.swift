c06d85870286aabfdee9bd78d25bc450515b34f3
kimjunseong <kimjunseong@archive-asia.co.jp>
2023-03-28 22:11:16 +0900

    add buttons

--- SwiftUI-TCA-Practice/ContentView.swift
+++ SwiftUI-TCA-Practice/ContentView.swift
@@ -6,21 +6,75 @@
 //
 
 import SwiftUI
+import ComposableArchitecture
+
+struct State: Equatable {
+    var counter = 0
+}
+
+enum Action: Equatable {
+    case increaseCounter
+    case decreaseCounter
+}
+
+struct Environment {
+    // Feature Dependencies..
+}
+
+let reducer = AnyReducer<State, Action, Environment> { state, action, environment in
+    switch action {
+    case .increaseCounter:
+        state.counter += 1
+        return .none
+    case .decreaseCounter:
+        state.counter -= 1
+        return .none
+    }
+}
 
 struct ContentView: View {
+    let store: Store<State, Action>
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        WithViewStore(self.store) { viewStore in
+            HStack {
+                Button {
+                    viewStore.send(.decreaseCounter)
+                } label: {
+                    Text("-")
+                        .padding(10)
+                        .background(.blue)
+                        .foregroundColor(.white)
+                        .cornerRadius(10)
+                }
+                .buttonStyle(.plain)
+                
+                Text(viewStore.counter.description)
+                    .padding(5)
+                
+                Button {
+                    viewStore.send(.increaseCounter)
+                } label: {
+                    Text("+")
+                        .padding(10)
+                        .background(.blue)
+                        .foregroundColor(.white)
+                        .cornerRadius(10)
+                }
+                .buttonStyle(.plain)
+            }
         }
-        .padding()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView(
+            store: Store(
+                initialState: State(),
+                reducer: reducer,
+                environment: Environment()
+            )
+        )
     }
 }
