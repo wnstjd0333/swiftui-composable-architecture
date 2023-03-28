300b81063c8dde7ceafe22d640bdceeb4cc80366
kimjunseong <kimjunseong@archive-asia.co.jp>
2023-03-28 21:23:23 +0900

    add: project

---
+++ SwiftUI-TCA-Practice/SwiftUI_TCA_PracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_TCA_PracticeApp.swift
+//  SwiftUI-TCA-Practice
+//
+//  Created by 金峻聖 on 2023/03/28.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_TCA_PracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
